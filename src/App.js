c7f4725d2776efddbc2bd9d85925dc25f00d59ec
Emrah Kolemenoglu <emperorain@gmail.com>
2022-06-20 16:44:48 +0300

    ADD:Detail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { BrowserRouter as Router,  Route } from "react-router-dom";
+import { BrowserRouter as Router ,Routes,Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from './components/Detail';
 import './App.css';
 
 function App() {
     return ( 
         <div className = "App" >
-           
-                    <Header / >
-                    <Home />
-              
+           <Router> 
+           <Header />
+                   <Routes>
+                   <Route path="/" element={<Home / >}/>
+                   <Route path="/" element={<Detail/ >}/>
+                   </Routes> 
+           </Router>
         </div >
     );
 }
---
+++ src/components/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+function Detail() {
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default Detail
